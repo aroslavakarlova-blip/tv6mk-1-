bb943b701b8901f9d79ddc761cc46846815c1946
aroslavakarlova-blip <aroslavakarlova@gmail.com>
2026-09-11 20:32:02 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'tv6mk1-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/icon-512.png'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
